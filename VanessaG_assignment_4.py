6040387b2233b1e1c791928e9b6a1bf2fe39b005
nxssabear <vanessacgray11@gmail.com>
2025-09-24 16:52:25 -0400

    Pass Test Case 2: Course planning with if/elif/else and comparison operators

--- VanessaG_assignment_4.py
+++ VanessaG_assignment_4.py
@@ -12,3 +12,28 @@ print(study_hours)
 print(social_points)
 print(stress_level)
 
+#test case 2:
+print("Choose the level of coursework you would like to take on this semester: ")
+print("(A) Easy (12 credits)")
+print("(B) Normal (15 credits)")
+print("(C) Hard (18 credits)")
+
+choice = input("Your choice: ")
+
+if choice == "A":
+    if current_gpa <= 2.5:
+        study_hours -= 10
+        stress_level += 5
+
+elif choice == "B":
+    if current_gpa >= 3.25:
+        study_hours += 5
+        stress_level += 10
+        
+elif choice == "C":
+    if current_gpa >= 3.75:
+        study_hours += 15
+        stress_level +=20
+else:
+    print("Invalid") 
+
